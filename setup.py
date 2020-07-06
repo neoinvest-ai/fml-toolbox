379f4e81cb4d9857d89dc0e87890f85260276698
Ayoub Ennassiri <ayo.enn@gmail.com>
2020-07-06 09:57:07 +0200

    Adding a license file, a requirements.txt and readme file

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-"""Setup.py module for the workflow's worker utilities.
+# Copyright 2020 Neoinvest.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Setup.py module for the workflow's worker utilities.
 All the workflow related code is gathered in a package that will be built as a
 source distribution, staged in the staging area for the workflow being run and
 then installed in the workers when they start running.
